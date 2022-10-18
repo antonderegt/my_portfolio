f9bf9fead03bb680fc51f2f6ad1806dc85169563
Anton De Regt <antonderegt@pm.me>
2022-10-18 16:10:57 +0200

    Extract header widget

---
+++ lib/widgets/header/header.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              "Welcome back 👋",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            Text(
+              "Anton's Portfolio",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        const Icon(
+          Icons.account_circle,
+          color: Colors.black,
+          size: 30,
+        ),
+      ],
+    );
+  }
+}
